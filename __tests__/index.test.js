fb8850bc1eb5e7a1a756b707761037967db8aca6
annaborovinskaya <annaborovin@gmail.com>
2022-06-08 12:59:32 +0300

    Step 4 fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,12 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import * as fs from 'fs';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const filename1 = fileURLToPath(import.meta.url);
+const dirname1 = dirname(filename1);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixture__', filename);
+const getFixturePath = (filename) => path.join(dirname1, '..', '__fixture__', filename);
 
 test('gendiff', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import _ from 'lodash';
 import fs from 'node:fs';
 import path from 'node:path';
-import cwd from 'node:process';
+import process from 'node:process';
 
 const genDiff = (filepath1, filepath2) => {
+  const readFile = (filepath) => (filepath.startsWith('/home') ? fs.readFileSync(filepath) : fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
 
-  const readFile = (filepath) => {
-	return filepath.startsWith('/home') ? fs.readFileSync(filepath) : fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
-  };
+  const file1 = JSON.parse(readFile(filepath1), 'utf-8');
+  const file2 = JSON.parse(readFile(filepath2), 'utf-8');
 
- const file1 = JSON.parse(readFile(filepath1), 'utf-8');
- const file2 = JSON.parse(readFile(filepath2), 'utf-8');	
- 
-const file1toArray = Object.keys(file1);
+  const file1toArray = Object.keys(file1);
   const file2toArray = Object.keys(file2);
   const uniqKeys = _.sortBy(_.union(file1toArray, file2toArray));
 
-  const obj = uniqKeys.reduce((obj, key) => {
+  const obj = uniqKeys.reduce((subobj, key) => {
+    /* eslint no-param-reassign: "error" */
     if (!_.has(file1, key)) {
-      obj[`+ ${key}`] = file2[key];
+      subobj[`+ ${key}`] = file2[key];
     }
     if (!_.has(file2, key)) {
-      obj[`- ${key}`] = file1[key];
+      subobj[`- ${key}`] = file1[key];
     }
     if (_.has(file1, key) && _.has(file2, key)) {
       if (file1[key] !== file2[key]) {
-        obj[`- ${key}`] = file1[key];
-        obj[`+ ${key}`] = file2[key];
+        subobj[`- ${key}`] = file1[key];
+        subobj[`+ ${key}`] = file2[key];
       }
       if (file1[key] === file2[key]) {
-        obj[`  ${key}`] = file1[key];
+        subobj[`  ${key}`] = file1[key];
       }
     }
-    return obj;
+    return subobj;
   }, {});
 
   const diff = `${_.keys(obj).reduce((acc, str) => {
